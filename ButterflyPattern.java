02d6a79b4d2ef7f75dbae28fa6d4ceb1f58b00d2
Meet kagathara <mrkagathara9909@gmail.com>
2025-05-04 06:54:56 +0530

    new Pattern program
    ButterflyPattern.java

--- ButterflyPattern.java
+++ ButterflyPattern.java
@@ -7,3 +7,40 @@
 // ***   ***
 // **     **
 // *       *
+
+import java.util.Scanner;
+
+public class ButterflyPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        // Upper half
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
