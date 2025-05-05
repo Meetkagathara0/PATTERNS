32c35f2e228b778ae48c9ed4bb01ac95c9bb5ef7
Meet kagathara <mrkagathara9909@gmail.com>
2025-05-05 15:37:04 +0530

    Update ConcentricSquareNumber.java

--- ConcentricSquareNumber.java
+++ ConcentricSquareNumber.java
@@ -8,5 +8,25 @@
 
 
 public class ConcentricSquareNumber {
-    
+    public static void main(String[] args) {
+        int n = 10; 
+        int size = 2 * n - 1;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                int top = i;
+                int left = j;
+                int bottom = size - 1 - i;
+                int right = size - 1 - j;
+
+                int min = top;
+                if(left < min) min = left;
+                if(bottom < min) min = bottom;
+                if(right < min) min = right;
+
+                System.out.print((n - min) + " ");
+            }
+            System.out.println();
+        }
+    }
 }
