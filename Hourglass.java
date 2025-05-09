ea20272015ca968bc68eb95fdb1791456281cad7
Meet kagathara <mrkagathara9909@gmail.com>
2025-05-09 07:07:32 +0530

    new pattern

--- Hourglass.java
+++ Hourglass.java
@@ -9,5 +9,36 @@
 // *********
 
 public class Hourglass {
-    
+    public static void main(String[] args) {
+        int n = 5;
+        int size = 2 * n - 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < size - 2 * i; j++) {
+                if (j == 0 || j == size - 2 * i - 1 || i == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < size - 2 * i; j++) {
+                if (j == 0 || j == size - 2 * i - 1 || i == 0) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
