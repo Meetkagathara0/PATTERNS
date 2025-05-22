7422d6fab39b0a54f31a8c6c6cfe6df5ca420334
Meet kagathara <mrkagathara9909@gmail.com>
2025-05-22 19:18:52 +0530

    Update PascalsTriangle.java

--- PascalsTriangle.java
+++ PascalsTriangle.java
@@ -1,10 +1,29 @@
-//     1      
-//    1 1     
-//   1 2 1    
-//  1 3 3 1   
-// 1 4 6 4 1  
+//      1      
+//     1 1     
+//    1 2 1    
+//   1 3 3 1   
+//  1 4 6 4 1  
 
 
 public class PascalsTriangle {
-    
+    public static void main(String[] args) {
+        int n = 6;
+        int[][] triangle = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int s = n - i; s > 1; s--) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    triangle[i][j] = 1;
+                } else {
+                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+                }
+                System.out.print(triangle[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
