f9ede5909080af1b3062f2643b2e37926ea65242
Meet kagathara <mrkagathara9909@gmail.com>
2025-06-09 22:16:49 +0530

    new pattern

--- NumberSpiral.java
+++ NumberSpiral.java
@@ -9,5 +9,46 @@
 // 13  12  11  10  9
 
 public class NumberSpiral {
-    
+    public static void main(String[] args) {
+        int n = 7;
+        int[][] matrix = new int[n][n];
+
+        int value = 1;
+        int top = 0, bottom = n - 1;
+        int left = 0, right = n - 1;
+
+        while (value <= n * n) {
+            // → left to right
+            for (int i = left; i <= right; i++) {
+                matrix[top][i] = value++;
+            }
+            top++;
+
+            // ↓ top to bottom
+            for (int i = top; i <= bottom; i++) {
+                matrix[i][right] = value++;
+            }
+            right--;
+
+            // ← right to left
+            for (int i = right; i >= left; i--) {
+                matrix[bottom][i] = value++;
+            }
+            bottom--;
+
+            // ↑ bottom to top
+            for (int i = bottom; i >= top; i--) {
+                matrix[i][left] = value++;
+            }
+            left++;
+        }
+
+        // Print matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.printf("%-4d", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
--- PalindromicNumberPyramid.java
+++ PalindromicNumberPyramid.java
@@ -6,5 +6,22 @@
 
 
 public class PalindromicNumberPyramid {
-    
+    public static void main(String[] args) {
+        int n = 6;
+        int temp = n;
+
+        for(int i=1; i<=n; i++){
+            for(int j=temp; j>1; j--){
+                System.out.print(" ");
+            }
+            temp--;
+            for(int k=1; k<=i; k++){
+                System.err.print(k);
+            }
+            for(int k=i-1; k>=1; k--){
+                if(i!=1) System.out.print(k);
+            }
+            System.out.println();
+        }
+    }
 }
